1dc2c416c2065edc0df48626d0ce218b34206f60
Omega <Omega@192.168.2.2>
2015-08-19 23:31:50 -0400

    Whole bunch of changes

--- src/lovebuddy/LoveBuddy.java
+++ src/lovebuddy/LoveBuddy.java
@@ -6,9 +6,13 @@
 package lovebuddy;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -18,6 +22,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.Keys;
 
 /**
  *
@@ -27,36 +32,98 @@ public class LoveBuddy {
 
     public WebDriver driver = new FirefoxDriver();
 
-    /**
-     * @param args the command line arguments
-     */
+    public void login() {
+        driver.navigate().to("https://www.tumblr.com/login");
+
+        WebElement userName_editbox = driver.findElement(By.id("signup_email"));
+        WebElement password_editbox = driver.findElement(By.id("signup_password"));
+        WebElement submit_button = driver.findElement(By.xpath(".//*[@id='signup_forms_submit']"));
+
+        userName_editbox.sendKeys("freshfresh91@gmail.com");
+        password_editbox.sendKeys("mastergeekz");
+        submit_button.click();
+
+    }
+
     public void openTestSite() {
-        driver.navigate().to("http://www.tumblr.com/search/rap");
+        driver.get("http://knowndoorsunknown.tumblr.com/notes/127047825926/kzie9mQE3?from_c=1439957501");
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");// new tab open
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "w");// close the tab
 
     }
 
-    public void findPopularBlog() {
-        driver.findElement(By.xpath(".//*[@id='search_posts']/article[2]/div/div[1]/div/div/span")).click();
+    public void scrollToBottom() {
+        //driver.findElement(By.xpath(".//*[@id='search_posts']/article[2]/div/div[1]/div/div/span")).click();
+        for (int x = 0; x < 1000; x++) {
+            try {
+                driver.findElement(By.xpath(".//*[@id='more_notes_127047825926']/a")).click();
+                Thread.currentThread().sleep(1000);
+            } catch (Exception e) {
+                System.out.println("Fail");
+            }
+
+        }
 
     }
 
-    public void addPeople() {
-        Actions dragger = new Actions(driver);
-        WebElement draggablePartOfScrollbar = driver.findElement(By.xpath(".//*[@id='search_posts']/article[2]/div/div[1]/div/div/div/div/div"));
-        
-       /* List<WebElement> allElements = driver.findElements(By.xpath(".//*[@id='search_posts']/article[2]/div/div[1]/div/div/div/div/div/div[1]/ol"));
+    public void getUsername() {
+        String userName = "";
+        Pattern pattern = Pattern.compile("http:\\/\\/(.*)\\.tumblr\\.com.*");
+        // Get matcher on this String.
+        Matcher m;
+        List<String> userNames = new ArrayList<>();
+        try {
+            for (int x = 1; x < 5; x++) {
+                m = pattern.matcher(driver.findElement(By.xpath("html/body/ol/li[" + x + "]/span/a[1]")).getAttribute("href"));
+                Thread.currentThread().sleep(1000);
 
-        for (WebElement element : allElements) {
-            System.out.println(element.getText());
-        }*/
+                // If it matches, get and display group values.
+                if (m.matches()) {
+                    userNames.add(m.group(1));// = m.group(1);
+                }
+
+                //driver.findElement(By.xpath("html/body/ol/li[1]/span/a[1]")).click(); //click on user name
+                Thread.currentThread().sleep(1000);
+                //  followUser(userName);
+            }
+        } catch (Exception e) {
+            System.out.println("Fail");
+        }
+
+        followUser(userNames);
+
+    }
+
+    public void followUser(List<String> userNames) {
+
+        userNames.stream().forEach((userName) -> {
+            System.out.println(userName);
+            driver.navigate().to("https://www.tumblr.com/search/" + userName);
+            if ("Follow".equals(driver.findElement(By.xpath(".//*[@id='search_actions_search']/div[4]/div/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div/button[2]")).getAttribute("class"))) {
+                System.out.println("System says: " + driver.findElement(By.xpath(".//*[@id='search_actions_search']/div[4]/div/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div/button[2]")).getText());
+
+                System.out.println("Followed");
+                driver.findElement(By.xpath(".//*[@id='search_actions_search']/div[4]/div/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div/button[2]")).click(); //click on user name
+                // driver.close();
+            } else {
+                System.out.println("System says: " + driver.findElement(By.xpath(".//*[@id='search_actions_search']/div[4]/div/div/div[2]/div[1]/div/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div/button[2]")).getAttribute("class"));
+
+                System.out.println("Dont Follow");
+                //  driver.close();
+
+            }
+
+        });
+
     }
 
     public static void main(String[] args) {
         // TODO code application logic here
         LoveBuddy webScrapper = new LoveBuddy(); //Here we are creating a instance of this class to make it a object from an idea 
+        webScrapper.login();
         webScrapper.openTestSite(); // Calling the method openTestSite which will excute the browser to be open
-        webScrapper.findPopularBlog();
-        webScrapper.addPeople();
+        // webScrapper.scrollToBottom();
+        webScrapper.getUsername();
     }
 
 }
